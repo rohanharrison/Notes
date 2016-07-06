5cf99de8a75908554bca6a0d1f81f715cde541e1
Rohan Harrison <roharriso@Hartford.edu>
2016-07-06 09:14:03 -0400

    Main Activity, Body
    Main Activity ListView & Body Activity Text

---
+++ app/src/main/java/com/rohanharrison/notes/MainActivity.java
@@ -0,0 +1,80 @@
+package com.rohanharrison.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String jsonResult;
+    private String url = "http://rohanharrison.com/message/androidLogin.php";
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        final ListView lv = (ListView) findViewById(R.id.notesList);
+
+        String[] noteTitle = new String[]{
+                "test",
+                "test2",
+                "test3"
+        };
+        final List<String> notes_list = new ArrayList<String>(Arrays.asList(noteTitle));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, notes_list);
+        lv.setAdapter(arrayAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Body.class);
+                startActivity(intent);
+                // notes_list.add("test");
+                //arrayAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
